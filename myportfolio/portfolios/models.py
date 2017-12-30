018b380361ab618124a7ccbc453d11401a48a9cd
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-12-30 04:51:28 +0300

    models - create Project class

--- myportfolio/portfolios/models.py
+++ myportfolio/portfolios/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Project(models.Model):
+    image = models.ImageField(upload_to='pictures/')
+    title = models.CharField(max_length=80)
+    description = models.TextField(max_length=300)
+    github_link = models.CharField(max_length=140)
+    deployed_link = models.CharField(max_length=140, blank=True)
+    active_project = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
